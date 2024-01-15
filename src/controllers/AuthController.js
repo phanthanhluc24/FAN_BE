18e1869c7fb566aa0167170617213b774d1c95c1
Phan Thanh Luc <luc.phan24@student.passerellesnumeriques.org>
2024-01-15 11:18:38 +0700

    feat(FAN-156): Write Api For Account Verification Code

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,5 +3,8 @@ class AuthController{
     async register(req,res){
         await AuthRepository.register(req,res)
     }
+    async verificationCode(req,res){
+        await AuthRepository.verificationCode(req,res)
+    }
 }
 module.exports=new AuthController()
