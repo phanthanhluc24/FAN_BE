c70d7bd2730b4ce93f405ea7594acc14818eda52
Phan Thanh Luc <luc.phan24@student.passerellesnumeriques.org>
2024-01-24 22:11:05 +0700

    fix: Get Repairman By Page

--- src/models/CategoriesModel.js
+++ src/models/CategoriesModel.js
@@ -2,7 +2,8 @@ const mongoose=require("mongoose")
 const Schema=mongoose.Schema
 
 const categoriesSchema=new Schema({
-    name:{type:String,require:true}
+    name:{type:String,require:true},
+    image:{type:String,require:true}
 },{
     timestamps:true
 })
